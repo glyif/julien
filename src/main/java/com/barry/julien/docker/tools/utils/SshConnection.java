8a7212fed62d94d64b12c8400bc870e2ce468db6
Bobby Yang <codecheffy@gmail.com>
2017-02-24 21:57:25 -0800

    ssh utility class

--- src/main/java/com/barry/julien/docker/tools/utils/SshConnection.java
+++ src/main/java/com/barry/julien/docker/tools/utils/SshConnection.java
@@ -3,9 +3,17 @@ package com.barry.julien.docker.tools.utils;
 
 import com.barry.julien.docker.Environment;
 import com.barry.julien.docker.tools.exception.JulienException;
+import com.barry.julien.docker.tools.response.CommandResponse;
+import com.jcraft.jsch.ChannelExec;
 import com.jcraft.jsch.JSchException;
 import com.jcraft.jsch.JSch;
 import com.jcraft.jsch.Session;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.stream.Collectors;
 
 /**
  * Created by gaoqingyang on 2/24/17.
@@ -47,10 +55,91 @@ public class SshConnection
      * @param session
      */
 
-    public static void closeSession(Session session) {
+    public static void close_session(Session session) {
         if (session != null) {
             session.disconnect();
         }
+    }
+
+    /**
+     * getString - returns the string
+     * @param is
+     * @return
+     * @throws JulienException
+     */
+
+    private static String getString(InputStream is) throws JulienException {
+        try (BufferedReader br = new BufferedReader(new InputStreamReader(is)))
+        {
+            return (br.lines().collect(Collectors.joining("\n")));
+        }
+        catch (IOException e)
+        {
+            throw new JulienException(e.getMessage());
+        }
+    }
+
+    /**
+     * executeCommand - takes a string and runs the command on a session
+     * @param session
+     * @param command
+     * @return
+     * @throws JulienException
+     */
+
+    public static CommandResponse executeCommand(Session session, String command) throws JulienException
+    {
+        try
+        {
+            ChannelExec channel = (ChannelExec) session.openChannel("exec");
+
+            channel.setCommand(command);
+
+            InputStream out;
+            InputStream error;
+
+            try {
+                out = channel.getInputStream();
+                error = channel.getErrStream();
+            }
+            catch (IOException e)
+            {
+                throw new JulienException(e.getMessage());
+            }
+
+            try {
+                out = channel.getInputStream();
+                error = channel.getErrStream();
+            } catch (IOException e) {
+                throw new JulienException(e.getMessage());
+            }
+
+            channel.connect();
+
+            CommandResponse response = CommandResponse.builder()
+                    .message(getString(out))
+                    .errMessage(getString(error)).build();
+
+            channel.disconnect();
+            return (response);
+        }
+        catch (JSchException e)
+        {
+            throw new JulienException(e.getMessage());
+
+        }
+    }
+
+    /**
+     * executeVoidCommand - gets error message of bad command
+     * @param session
+     * @param command
+     * @return
+     * @throws JulienException
+     */
+    public static String executeVoidCommand(Session session, String command) throws JulienException
+    {
+        return (executeCommand(session, command).getErrMessage());
     }
 
 
